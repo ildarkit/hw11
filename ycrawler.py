6924d2a752f35439a94f9e69de425bf33e4a925b
ILdar Shamiev <shamief@yandex.ru>
2018-04-06 18:24:19 +0300

    add download coroutine

--- ycrawler.py
+++ ycrawler.py
@@ -14,7 +14,7 @@ import async_timeout
 
 
 TOP_STORIES_URL = "https://news.ycombinator.com/"
-FETCH_TIMEOUT = 3
+FETCH_TIMEOUT = 10
 NEWS_QUERY_SUBSTR = 'item?id='
 
 
@@ -31,6 +31,7 @@ class URLFetcher():
         """
         with async_timeout.timeout(FETCH_TIMEOUT):
             async with session.get(url) as response:
+                self.fetch_counter += 1
                 return await response.text()
 
 
@@ -46,7 +47,7 @@ def get_links(html, right='"', substr=None):
     return new_urls
 
 
-def get_top_news_links(links, limit):
+def get_full_links(links, root, limit):
 
     # удаление дубликатов
     unique_urls = set(links)
@@ -59,7 +60,7 @@ def get_top_news_links(links, limit):
     if limit:
         urls = urls[:limit]
 
-    return [urljoin(TOP_STORIES_URL, remove_fragment(new_url)) for new_url in urls]
+    return [urljoin(root, remove_fragment(new_url)) for new_url in urls]
 
 
 def remove_fragment(url):
@@ -67,74 +68,80 @@ def remove_fragment(url):
     return pure_url
 
 
-async def saving(path, body):
+def writing(path, body):
     try:
-        with open(path, 'wb') as f:
+        with open(path, 'w') as f:
             f.write(body)
     except (PermissionError, IOError):
-        pass
+        raise
 
 
-async def get_comments_page(loop, session, fetcher, link, save_dir, story=False):
-    """Retrieve data for current post and recursively for all comments.
+async def download(loop, session, fetcher, link, path):
+
+    try:
+        response = await fetcher.fetch(session, link)
+    except Exception as e:
+        logging.debug("Error retrieving post {}: {}".format(link, e))
+        raise e
+
+    try:
+        await loop.run_in_executor(ThreadPoolExecutor, writing, path, response)
+    except Exception as e:
+        logging.debug("Error retrieving saving new sites: {}".format(e))
+        raise
+
+
+async def get_comments_urls(loop, session, fetcher, link, save_dir, story=False):
+    """Retrieve data for current post.
     """
-    news_id = link.split(NEWS_QUERY_SUBSTR)[1]
-    news_dir = os.path.abspath(os.path.join(save_dir, news_id))
+    try:
+        response = await fetcher.fetch(session, link)
+    except Exception as e:
+        logging.debug("Error retrieving post {}: {}".format(link, e))
+        raise e
 
-    if not os.path.exists(news_dir):
+    news_id = link.split(NEWS_QUERY_SUBSTR)[1]
+    save_dir = os.path.abspath(os.path.join(save_dir, news_id))
+    comments_links = []
+
+    if not os.path.exists(save_dir):
         try:
-            os.makedirs(news_dir)
+            os.makedirs(save_dir)
         except (PermissionError, IOError) as e:
             tb_lines = traceback.format_exception(*sys.exc_info())
             logging.exception(''.join(tb_lines))
             raise e
 
-        try:
-            response = await fetcher.fetch(session, link)
-        except Exception as e:
-            logging.debug("Error retrieving post {}: {}".format(link, e))
-            raise e
-
-        try:
-            await loop.run_in_executor(ThreadPoolExecutor, saving, news_dir, response)
-        except Exception as e:
-            logging.debug("Error retrieving saving new sites: {}".format(e))
-            raise
-
+        links = []
         if story:
             links = get_links(response, right='" class="storylink"')
-        else:
-            links = get_links(response)
 
-        # calculate this post's comments as number of comments
-        number_of_comments = len(response['kids'])
+        comments_links = get_links(response, substr='http')
+        links.extend(comments_links)
 
         try:
-            # create recursive tasks for all comments
-            tasks = [asyncio.ensure_future(get_comments_page(
-                loop, session, fetcher, link)) for link in links]
+            tasks = [asyncio.ensure_future(
+                download(loop, session, fetcher, link, save_dir)
+            ) for link in links]
 
-            # schedule the tasks and retrieve results
+            # schedule the tasks
             try:
-                results = await asyncio.gather(*tasks)
+                await asyncio.gather(*tasks)
             except Exception as e:
                 logging.debug("Error retrieving comments for top stories: {}".format(e))
                 raise
 
-            # reduce the descendents comments and add it to this post's
-            number_of_comments += sum(results)
-            logging.debug('{:^6} > {} comments'.format(post_id, number_of_comments))
-
-            return number_of_comments
         except asyncio.CancelledError:
             if tasks:
                 logging.info("Comments for post {} cancelled, cancelling {} child tasks".format(
-                    post_id, len(tasks)))
+                    news_id, len(tasks)))
                 for task in tasks:
                     task.cancel()
             else:
-                logging.info("Comments for post {} cancelled".format(post_id))
+                logging.info("Comments for post {} cancelled".format(news_id))
             raise
+
+    return len(comments_links)
 
 
 async def get_news(loop, session, limit, iteration, save_dir):
@@ -147,17 +154,19 @@ async def get_news(loop, session, limit, iteration, save_dir):
         logging.error("Error retrieving top stories: {}".format(e))
         # return instead of re-raising as it will go unnoticed
         return
-    except Exception as e:  # catch generic exceptions
-        logging.error("Unexpected exception: {}".format(e))
-        return
 
     # получение ссылок на страницы комментариев
-    links = get_top_news_links(get_links(response, substr=NEWS_QUERY_SUBSTR), limit=limit)
+    links = get_full_links(
+        get_links(response, substr=NEWS_QUERY_SUBSTR),
+        TOP_STORIES_URL, limit=limit
+    )
 
     tasks = {
         asyncio.ensure_future(
-            get_comments_page(loop, session, fetcher, link, save_dir, story=True)
-        ): link for link in links}
+            get_comments_urls(
+                loop, session, fetcher, link, save_dir, story=True
+            )): link for link in links
+    }
 
     # return on first exception to cancel any pending tasks
     done, pending = await asyncio.shield(asyncio.wait(
@@ -175,7 +184,7 @@ async def get_news(loop, session, limit, iteration, save_dir):
             print("Post {} has {} comments ({})".format(
                 tasks[done_task], done_task.result(), iteration))
         except Exception as e:
-            print("Error retrieving comments for top stories: {}".format(e))
+            print("Error retrieving top stories: {}".format(e))
 
     return fetcher.fetch_counter
 
@@ -190,7 +199,7 @@ async def poll_top_news(loop, session, period, limit, save_dir):
             logging.info('Error detected, quitting')
             return
 
-        logging.info("Calculating comments for top {} stories. ({})".format(
+        logging.info("Top {} news processing. ({})".format(
             limit, iteration))
 
         future = asyncio.ensure_future(
@@ -203,10 +212,6 @@ async def poll_top_news(loop, session, period, limit, save_dir):
             try:
                 fetch_count = fut.result()
             except Exception as e:
-                logging.debug('Adding {} to errors'.format(e))
-                errors.append(e)
-            except Exception as e:
-                logging.exception('Unexpected error')
                 errors.append(e)
             else:
                 logging.info(
@@ -227,9 +232,10 @@ async def run(loop, period, limit, save_dir):
 
 if __name__ == '__main__':
     op = OptionParser()
+    op.add_option("-L", "--log", action="store", default="")
     op.add_option("-l", "--limit", action="store", default=30, type="int")
     op.add_option("-p", "--period", action="store", default=10, type="int")
-    op.add_option("-d", "--downloads", action="store", default="./downloads/")
+    op.add_option("-d", "--downloads", action="store", default='D:\\otus_python\\hw11\\downloads\\')
     (opts, args) = op.parse_args()
     logging.basicConfig(filename=opts.log, level=logging.INFO,
                         format='[%(asctime)s] %(levelname).1s %(message)s', datefmt='%Y.%m.%d %H:%M:%S')
